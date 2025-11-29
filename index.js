25f51063b6f19168abb0ffc535d4098f29c109f0
Balu <kharadeb999@gmail.com>
2025-11-29 08:41:00 +0530

    express-session

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const Users = require("./db/Users");
 const { verifyToken, getJwtToken } = require("./utils/jwt-token");
 const { mainRoute } = require("./routes");
+const session = require("express-session");
 require("dotenv").config();
 require("./db/config");
 
@@ -17,7 +18,14 @@ app.use(
 app.use(express.json());
 
 let userInfo = {};
-
+app.use(
+  session({
+    secret: "Sairaj",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: true, maxAge: 60000 * 60 },
+  })
+);
 app.use(mainRoute);
 
 app.get("/isLoggendIn", verifyToken, (req, resp, next) => {
@@ -26,13 +34,12 @@ app.get("/isLoggendIn", verifyToken, (req, resp, next) => {
 
 app.post("/login", async (req, resp, next) => {
   const user = await Users.findOne(req.body);
-
   if (user && !user?.deleted) {
     if (userInfo?._id) {
-      resp.send({ user, getJwtToken });
+      resp.send({ user, token: getJwtToken() });
     } else {
       userInfo = user;
-      resp.send({ user, getJwtToken });
+      resp.send({ user, token: getJwtToken() });
     }
   } else {
     try {
